1092ffb7e477fe0324be98bf470c25696ace04c0
khoshghalb2c <khoshghalb2c@gmail.com>
2022-09-24 10:54:24 +0330

    Update main.js

--- main.js
+++ main.js
@@ -1,30 +1,22 @@
 /*!
-  * Scopy > Owner is https://github.com/khoshghalb2c;
-  * Copyright 2020-2021;
-  */
-  function Scopy(text){
-    copyTextToClipboard(text);
-  }
-  function copyTextToClipboard(text){
-    if(!navigator.clipboard){
-      fallbackCopyTextToClipboard(text);
-      return;
-    }
-    navigator.clipboard.writeText(text).then(function(){}, function(err){});
-  }
-  function fallbackCopyTextToClipboard(text){
-    var textArea = document.createElement("textarea");
-    textArea.value = text; 
-    textArea.style.top = "0";
-    textArea.style.left = "0"; 
-    textArea.style.position = "fixed";
-    document.body.appendChild(textArea);
-    textArea.focus(); 
-    textArea.select();
-    try{
-      var successful = document.execCommand('copy'); 
-      var msg = successful ? 'successful' : 'unsuccessful';
-    }catch(err){} 
-    document.body.removeChild(textArea);
-  }
+ * Scopy > Owner is https://github.com/khoshghalb2c;
+ * Copyright 2020-2021;
+ */
+ function Scopy(text){
+  if(!navigator.clipboard)
+    fallbackCopyTextToClipboard(text);
+  else 
+   navigator.clipboard.writeText(text).then(function(){}, function(err){});
+ }
+ function fallbackCopyTextToClipboard(text){
+  var textArea = document.createElement("textarea");
+  textArea.value = text; 
+  textArea.style.display = "none";
+  document.body.appendChild(textArea);
+  textArea.focus(); 
+  textArea.select();
+  textArea.setSelectionRange(0, 99999);
+  textArea.clipboard.writeText(textArea.value);
+  document.body.removeChild(textArea);
+ }
 /*!* POWERED BY khoshghalb2c.ir *!*/
